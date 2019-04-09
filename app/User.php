cf8c254618b8ebc3838f6dabd28d21e9a6e355e1
HARI HARA SUDAAN KRISHNEN <hariharasudaankrishnen@gmail.com>
2019-04-09 16:11:16 -0400

    Feature: Added Question migration, model and made user seeder and added tests for user profile (cleanup), and question test

--- app/User.php
+++ app/User.php
@@ -68,4 +68,8 @@ class User extends Authenticatable
     public function profile() {
         return $this->hasOne('App\Profile');
     }
+
+    public function questions() {
+        return $this->hasMany('App\Question');
+    }
 }
