65fdae76b83ddff06bf2ff4cfa17184630f26e4a
HARI HARA SUDAAN KRISHNEN <hariharasudaankrishnen@gmail.com>
2019-04-10 01:22:19 -0400

    Feature: Edit of the user profile along with the validations

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Profile;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -78,9 +79,13 @@ class ProfileController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($user, $profile)
     {
-        //
+        $user = User::find($user);
+        $profile = $user->profile;
+        $edit = TRUE;
+
+        return view('profileForm', ['profile' => $profile, 'edit' => $edit ]);
     }
 
     /**
@@ -90,9 +95,24 @@ class ProfileController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request,  $user, $profile)
     {
-        //
+        $input = $request->validate([
+            'fname' => 'required',
+            'lname' => 'required',
+        ], [
+
+            'fname.required' => ' First is required',
+            'lname.required' => ' Last is required',
+
+        ]);
+        $profile = Profile::find($profile);
+        $profile->fname = $request->fname;
+        $profile->lname = $request->lname;
+        $profile->body = $request->body;
+        $profile->save();
+
+        return redirect()->route('home')->with('message', 'Updated Profile');
     }
 
     /**
