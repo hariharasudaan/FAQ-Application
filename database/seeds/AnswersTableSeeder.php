9a7fd999e5ac7797670f52293f2862b646ab1e2f
HARI HARA SUDAAN KRISHNEN <hariharasudaankrishnen@gmail.com>
2019-04-09 20:07:07 -0400

    Feature: Added the profile view for displaying the profile data

--- database/seeds/AnswersTableSeeder.php
+++ database/seeds/AnswersTableSeeder.php
@@ -14,7 +14,7 @@ class AnswersTableSeeder extends Seeder
     public function run()
     {
         $users = App\User::inRandomOrder();
-        //for ($i = 1; $i <= 6; $i++) {
+        for ($i = 1; $i <= 6; $i++) {
             $users->each(function ($user) {
                 $question = App\Question::inRandomOrder()->first();
                 $answer = factory(\App\Answer::class)->make();
@@ -22,6 +22,6 @@ class AnswersTableSeeder extends Seeder
                 $answer->question()->associate($question);
                 $answer->save();
             });
-        //}
+        }
     }
 }
